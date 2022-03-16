647b504cc5e9e421ce87dad48d201707afdb0e29
Olga Volosevich <o.volosevich96@gmail.com>
2022-03-17 02:51:30 +0300

    init: specify webpack config for dev & prod

---
+++ webpack/webpack.common.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "..", "src/index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "..", "dist"),
+    filename: "bundle.js",
+  },
+  // devServer: {
+  //   static: {
+  //     directory: path.join(__dirname, "..", "./dist"),
+  //   },
+  //   compress: true,
+  //   port: 9000,
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "..", "src", "index.html"),
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "**/*",
+          context: path.resolve(__dirname, "..", "src", "components"),
+          to: "./components",
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};

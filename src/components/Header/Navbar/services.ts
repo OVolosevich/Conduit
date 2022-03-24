e6536fb83829268d0858215eabda555759e3dd06
Olga Volosevich <o.volosevich96@gmail.com>
2022-03-24 23:02:28 +0300

    refactor: add output type for getLinksForNavbar fn

--- src/components/Header/Navbar/services.ts
+++ src/components/Header/Navbar/services.ts
@@ -1,6 +1,6 @@
 const linksForGuest: string[] = ["home", "sign in", "sign up"];
 const linksForUser: string[] = ["home", "new article", "settings", "username"];
 
-const getLinksForNavbar = (isVisitorInSystem: boolean) => isVisitorInSystem ? linksForUser : linksForGuest;
+const getLinksForNavbar = (isVisitorInSystem: boolean): string[] => isVisitorInSystem ? linksForUser : linksForGuest;
 
 export { getLinksForNavbar, linksForGuest, linksForUser };

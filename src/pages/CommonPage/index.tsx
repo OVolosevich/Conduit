05ab247784d89a6fe8683adca6f8ea3444facd5a
Olga Volosevich <o.volosevich96@gmail.com>
2022-03-27 17:41:35 +0300

    feat: create CommonPage layout

---
+++ src/pages/CommonPage/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+const CommonPage: React.FC = () => (
+  <>
+    <Header />
+    <main>
+      main content
+    </main>
+    <footer>
+      footer
+    </footer>
+  </>
+);
+
+export default CommonPage;

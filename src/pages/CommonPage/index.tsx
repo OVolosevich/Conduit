d9789773a0d6a2d3bbee6661113e782dc6a8f62b
Olga Volosevich <o.volosevich96@gmail.com>
2022-03-27 19:46:02 +0300

    styles: add stylization to Footer

--- src/pages/CommonPage/index.tsx
+++ src/pages/CommonPage/index.tsx
@@ -3,13 +3,13 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
 const CommonPage: React.FC = () => (
-	<>
-		<Header />
-		<main>
-			main content
-		</main>
-		<Footer />
-	</>
+  <>
+    <Header />
+    <main>
+      main content
+    </main>
+    <Footer />
+  </>
 );
 
 export default CommonPage;

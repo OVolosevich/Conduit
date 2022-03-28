4c9d130d49088da57973967c4efc95e003af032c
Olga Volosevich <o.volosevich96@gmail.com>
2022-03-28 21:49:17 +0300

    refactor: restore Outlet component

--- src/pages/CommonPage/index.tsx
+++ src/pages/CommonPage/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import { Outlet } from 'react-router-dom';
 
 const CommonPage: React.FC = () => (
   <>
     <Header />
     <main>
-      main content
+      <Outlet />
     </main>
     <Footer />
   </>

e796d4c068e7541c5536efb4611f828eb58ac827
Olga Volosevich <o.volosevich96@gmail.com>
2022-03-28 22:29:10 +0300

    feat: add Banner to HomePage

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const Banner: React.FC = () => {
+	return (
+		<div className={styles.banner}>
+			<h1 className={styles.header}>Conduit</h1>
+			<p className={styles["banner__moto"]}>A place to share your knowledge.</p>
+		</div>
+	)
+};
+
+export default Banner;
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Banner from '../../components/Banner';
 
-const HomePage: React.FC = () => <div>homepage</div>;
+const HomePage: React.FC = () => {
+	return (
+		<div>
+			<Banner />
+		</div>
+	)
+};
 
 export default HomePage;

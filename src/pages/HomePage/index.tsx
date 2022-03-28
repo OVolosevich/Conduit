474dcccfc05cb5d740c6b5727d3d38c7aedd50c5
Olga Volosevich <o.volosevich96@gmail.com>
2022-03-28 22:29:40 +0300

    refactor: add lint fixes

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Banner from '../../components/Banner';
 
-const HomePage: React.FC = () => {
-	return (
-		<div>
-			<Banner />
-		</div>
-	)
-};
+const HomePage: React.FC = () => (
+  <div>
+    <Banner />
+  </div>
+);
 
 export default HomePage;

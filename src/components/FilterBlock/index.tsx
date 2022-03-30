55a298b49cee51b7f7ad7244042151dd0f04f654
Olga Volosevich <o.volosevich@freshlimesoft.com>
2022-03-30 12:53:03 +0300

    refactor: move useState from FilterBlock to FilterTabs

--- src/components/FilterBlock/index.tsx
+++ src/components/FilterBlock/index.tsx
@@ -1,22 +1,18 @@
-import React, { useState } from 'react';
-import FilterTabs from './FilterTabs';
-import FilterBlockItemList from './FilterBlockItemList';
-import { articleData } from './data';
-import { tagsList, unregisteredGuestData } from '../../variables';
-import TagsBar from './TagBar';
-import styles from './styles.module.css';
+import React, { useState } from "react";
+import FilterTabs from "./FilterTabs";
+import FilterBlockItemList from "./FilterBlockItemList";
+import { articleData } from "./data";
+import { tagsList, unregisteredGuestData } from "../../variables";
+import TagsBar from "./TagBar";
+import styles from "./styles.module.css";
 
 const FilterBlock: React.FC = () => {
-  const [defaultTabs, setDefaultTabs] = useState<string[]>(unregisteredGuestData.filterBlockTabs);
-  const [chosenTab, setChosenTab] = useState<string>(unregisteredGuestData.filterBlockTabs[0]);
-  const allTabs = new Set([...defaultTabs, chosenTab]);
+  const [chosenTab, setChosenTab] = useState<string>(
+    unregisteredGuestData.filterBlockTabs[0]
+  );
   return (
-    <div className={styles['filter-block']}>
-      <FilterTabs
-        setDefaultTabs={setDefaultTabs}
-        setChosenTab={setChosenTab}
-        tabs={Array.from(allTabs)}
-      />
+    <div className={styles["filter-block"]}>
+      <FilterTabs setChosenTab={setChosenTab} chosenTab={chosenTab} />
       <FilterBlockItemList articles={articleData} />
       <TagsBar setTab={setChosenTab} tagsList={tagsList} />
     </div>

d06f42002bd395e32b9cdf6f676bf4bb50148b4f
Olga Volosevich <o.volosevich@freshlimesoft.com>
2022-04-01 17:40:05 +0300

    refactor: add checking articlesList before rendering FilterBlockItemList

--- src/components/FilterBlock/index.tsx
+++ src/components/FilterBlock/index.tsx
@@ -1,31 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { tagsList, unregisteredGuestData } from '../../variables';
-import { getFilteredArticles } from './services';
-import FilterTabs from './FilterTabs';
-import FilterBlockItemList from './FilterBlockItemList';
-import TagsBar from './TagBar';
-import styles from './styles.module.css';
-import { ArticleItem } from '../../Shared';
+import React, { useState } from "react";
+import { tagsList, unregisteredGuestData } from "../../variables";
+import { getFilteredArticles } from "./services";
+import FilterTabs from "./FilterTabs";
+import FilterBlockItemList from "./FilterBlockItemList";
+import TagsBar from "./TagBar";
+import styles from "./styles.module.css";
+import { ArticleItem } from "../../Shared";
 
 interface FilterBlockProps {
-  articlesList: ArticleItem[]
+  articlesList: ArticleItem[] | undefined;
 }
 
 const FilterBlock: React.FC<FilterBlockProps> = (props) => {
   const { articlesList } = props;
   const [chosenTab, setChosenTab] = useState<string>(
-    unregisteredGuestData.filterBlockTabs[0],
+    unregisteredGuestData.filterBlockTabs[0]
   );
-  const [areArticlesPresent, setArticlesPresent] = useState<boolean>(false);
-  useEffect(() => {
-    setArticlesPresent(true);
-  }, [articlesList]);
+
   return (
-    <div className={styles['filter-block']}>
+    <div className={styles["filter-block"]}>
       <FilterTabs setChosenTab={setChosenTab} chosenTab={chosenTab} />
-      {areArticlesPresent
-        ? <FilterBlockItemList articles={getFilteredArticles(articlesList, chosenTab)} />
-        : 'Loading'}
+      {articlesList && articlesList.length > 0 ? (
+        <FilterBlockItemList
+          articles={getFilteredArticles(articlesList!, chosenTab)}
+        />
+      ) : (
+        "Loading"
+      )}
       <TagsBar setTab={setChosenTab} tagsList={tagsList} />
     </div>
   );

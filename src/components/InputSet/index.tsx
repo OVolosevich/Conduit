7fac87bc75a42d342de6166f6481e4c7251ed6bb
Olga Volosevich <o.volosevich@freshlimesoft.com>
2022-06-29 16:56:57 +0300

    feat: add validation to sign-up form

--- src/components/InputSet/index.tsx
+++ src/components/InputSet/index.tsx
@@ -11,12 +11,45 @@ interface InputSetProps {
   item: Input;
   value: string;
   setValue: React.Dispatch<React.SetStateAction<InputsStatesSignature>>;
+  setErrors: React.Dispatch<React.SetStateAction<string[]>>;
+  errors: string[];
 }
 
-const InputSet: React.FC<InputSetProps> = ({ item, value, setValue }) => {
+const InputSet: React.FC<InputSetProps> = ({
+  item,
+  value,
+  setValue,
+  setErrors,
+  errors,
+}) => {
   const {
-    type, name, label, labelId,
+    type, name, label, labelId, isValidated, regExp,
   } = item;
+  const validateInput = (
+    expressionToMath: {
+      [Symbol.match](string: string): RegExpMatchArray | null;
+    },
+    event: React.FocusEvent<HTMLInputElement>,
+  ): boolean => (!!(event.target.value && event.target.value.match(expressionToMath)));
+
+  const blurHandler = (
+    expressionToMath: {
+      [Symbol.match](string: string): RegExpMatchArray | null;
+    },
+    event: React.FocusEvent<HTMLInputElement>,
+  ): void => {
+    const isValueValid = validateInput(expressionToMath, event);
+    if (isValueValid === false) {
+      const warning = `${event.target.name} is invalid`;
+      if (!errors.includes(warning)) {
+        setErrors((prev) => [...prev, warning]);
+      }
+    }
+  };
+
+  const focusHandler = (event: React.FocusEvent<HTMLInputElement>): void => {
+    setErrors((prev) => [...prev].filter((warning) => !warning.includes(event.target.name)));
+  };
   return (
     <div className={styles['input-set']} key={labelId}>
       <label htmlFor={labelId}>{label}</label>
@@ -25,7 +58,9 @@ const InputSet: React.FC<InputSetProps> = ({ item, value, setValue }) => {
         type={type}
         name={name}
         value={value}
-        onChange={(e) => setValue((prev) => ({ ...prev, [name]: e.target.value }))}
+        onChange={(event) => setValue((prev) => ({ ...prev, [name]: event.target.value }))}
+        onFocus={(event) => isValidated && focusHandler(event)}
+        onBlur={(event) => isValidated && regExp && blurHandler(regExp, event)}
       />
     </div>
   );

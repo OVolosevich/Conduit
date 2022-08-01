89f2ebdd604597e4937328fc7fd83da2fe0c7bfd
Olga Volosevich <o.volosevich96@gmail.com>
2022-08-01 21:12:09 +0300

    fix: lint fixes

--- src/variables.ts
+++ src/variables.ts
@@ -7,7 +7,7 @@ export interface Input {
   isValidated: boolean;
 }
 
-interface userInterfaceData {
+interface UserInterfaceData {
   navbarLinks: string[];
   filterBlockTabs: string[];
   signUpInputs?: Input[];
@@ -18,12 +18,12 @@ const inputsValidation = {
   email:
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 };
-export const registeredUserData: userInterfaceData = {
+export const registeredUserData: UserInterfaceData = {
   navbarLinks: ['home', 'new article', 'settings', 'username'],
   filterBlockTabs: ['your feed', 'global feed'],
 };
 
-export const unregisteredGuestData: userInterfaceData = {
+export const unregisteredGuestData: UserInterfaceData = {
   navbarLinks: ['home', 'sign in', 'sign up'],
   filterBlockTabs: ['global feed'],
   signUpInputs: [

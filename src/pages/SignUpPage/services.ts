d76ebd26c57681ed27fef22d927eb61b702ca36d
Olga Volosevich <o.volosevich96@gmail.com>
2022-08-10 21:54:33 +0300

    fix: correct formatting for SignUpPage reducer

--- src/pages/SignUpPage/services.ts
+++ src/pages/SignUpPage/services.ts
@@ -2,29 +2,29 @@ import { UserInfo, UserInputAction } from '../../Shared';
 import { emptySignUpFormInputs } from '../../variables';
 
 const enum UserInputActionTypes {
-	cleanForm = 'clean',
-	changeUsername = 'username',
-	changePassword = 'password',
-	changeEmail = 'email',
+  cleanForm = 'clean',
+  changeUsername = 'username',
+  changePassword = 'password',
+  changeEmail = 'email',
 }
 
 export default function reducer(state: UserInfo, action: UserInputAction): UserInfo {
-	switch (action.type) {
-		case UserInputActionTypes.cleanForm:
-			return { ...emptySignUpFormInputs };
-		case UserInputActionTypes.changeUsername:
-			return action.payload
-				? { ...state, username: action.payload }
-				: { ...state, username: '' };
-		case UserInputActionTypes.changeEmail:
-			return action.payload
-				? { ...state, email: action.payload }
-				: { ...state, email: '' };
-		case UserInputActionTypes.changePassword:
-			return action.payload
-				? { ...state, password: action.payload }
-				: { ...state, password: '' };
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case UserInputActionTypes.cleanForm:
+      return { ...emptySignUpFormInputs };
+    case UserInputActionTypes.changeUsername:
+      return action.payload
+        ? { ...state, username: action.payload }
+        : { ...state, username: '' };
+    case UserInputActionTypes.changeEmail:
+      return action.payload
+        ? { ...state, email: action.payload }
+        : { ...state, email: '' };
+    case UserInputActionTypes.changePassword:
+      return action.payload
+        ? { ...state, password: action.payload }
+        : { ...state, password: '' };
+    default:
+      return state;
+  }
 }
